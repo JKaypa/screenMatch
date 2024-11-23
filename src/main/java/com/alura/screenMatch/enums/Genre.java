efdafe1a35c227dd1d0d64a0403b3a9866533e07
JKaypa <jose_kaypa@hotmail.com>
2024-11-23 11:49:38 -0500

    create Genre enum

--- src/main/java/com/alura/screenMatch/enums/Genre.java
+++ src/main/java/com/alura/screenMatch/enums/Genre.java
@@ -1,4 +1,23 @@
 package com.alura.screenMatch.enums;
 
 public enum Genre {
+    ACTION("Action"),
+    ADVENTURE("Adventure"),
+    COMEDY("Comedy"),
+    CRIME("Crime"),
+    DRAMA("Drama"),
+    FANTASY("Fantasy");
+
+
+    Genre(String name) {}
+
+    public static Genre fromString (String genre) {
+        for (Genre value : Genre.values()) {
+            if (value.name().equalsIgnoreCase(genre)) {
+                return value;
+            }
+        }
+
+        throw new IllegalArgumentException("Genre not found! - " + genre);
+    }
 }
