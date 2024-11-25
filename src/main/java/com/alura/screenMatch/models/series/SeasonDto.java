eefab1c7a99fcc07c01d5b84491b7d88918811c9
JKaypa <jose_kaypa@hotmail.com>
2024-11-25 16:31:54 -0500

    change season type

--- src/main/java/com/alura/screenMatch/models/series/SeasonDto.java
+++ src/main/java/com/alura/screenMatch/models/series/SeasonDto.java
@@ -7,5 +7,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record SeasonDto(@JsonAlias("Season") String season,
-                               @JsonAlias("Episodes")List<EpisodeDto> episodes) {}
+public record SeasonDto(
+        @JsonAlias("Season") int season,
+        @JsonAlias("Episodes")List<EpisodeDto> episodes) {}
